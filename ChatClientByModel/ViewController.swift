d469556c339c58e850de58e3c70200470c610dcd
rolodestar <39199934@qq.com>
2020-03-08 10:46:18 +0800

    Initial Commit

---
+++ ChatClientByModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatClientByModel
+//
+//  Created by rolodestar on 2020/3/8.
+//  Copyright © 2020 Rolodestar Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
